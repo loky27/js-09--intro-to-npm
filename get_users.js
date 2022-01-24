4d1bb15fc6b6e966a7f2eb1eedf5989a7bf0efe9
jonatha <hef6666@gmail.com>
2022-01-24 10:26:05 -0600

    adelanto

--- get_users.js
+++ get_users.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
-async function makeGetRequest() {
-  
-  // let res = await ;
-
-  let data = res.data;
-  console.log(data);
+function makeGetRequest() {
+  axios.get(' http://localhost:3000/users',{responseType:'json'}).then(response => {
+    const colorsJson = JSON.stringify(response.data)
+    console.log(colorsJson)
+})
 }
+
 
 makeGetRequest();
