eef5717fd92a8123210a9e6da6d4fb9fd1a397e7
jonatha <hef6666@gmail.com>
2022-01-25 09:19:15 -0600

    terminado

--- post_user.js
+++ post_user.js
@@ -2,12 +2,17 @@ const axios = require('axios');
 
 async function makePostRequest() {
 
-    params = {
-        id: 6,
-        first_name: 'Fred',
-        last_name: 'Blair',
-        email: 'freddyb34@gmail.com'
-      }
+  
+      let res = await axios({
+        method: 'post',
+        url: 'http://localhost:3000/users',
+        data: {
+          id: 6,
+    first_name: 'Fred',
+    last_name: 'Blair',
+    email: 'freddyb34@gmail.com'
+        }
+      })
 
       //let res = await ;
 
